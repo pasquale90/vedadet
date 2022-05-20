d4377f9fb1a00d40b67c96598da5ecd52d7db806
pasquale90 <melissaspaschalis@gmail.com>
2022-05-20 13:01:44 +0300

    added -y to automate installation

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,6 +5,6 @@ env_name=tinaface7
 conda create -n ${env_name} python=3.7 -y
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ${env_name}
-conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
+conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch -y
 pip install -r requirements/build.txt
 pip install -v -e .
